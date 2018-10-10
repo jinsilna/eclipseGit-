237dea4f5a89921319a082f4225e02e5dec1c003
najinsil <sally1334@naver.com>
2018-10-10 17:46:59 +0900

    second commit

--- src/main/java/kr/or/ddit/web/BasicServlet.java
+++ src/main/java/kr/or/ddit/web/BasicServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// 이클립스 git second commit 
 
 /**
  * Servlet implementation class BasicServlet
